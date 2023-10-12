ac8120fa7222145b4bcb9b0fbdee4b9d91878998
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-12 19:11:40 +0530

    Operators in java

---
+++ Operators/arithmetic.java
@@ -0,0 +1,12 @@
+//Arithmetic operator in java
+package Operators;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(4-1);
+        System.out.println(3*2);
+        System.out.println(10/5);
+        System.out.println(10%3);
+    }
+}
