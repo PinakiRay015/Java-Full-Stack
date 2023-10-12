33c0e7672608fe0ad20feb1880b7042227572dd5
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-12 20:01:50 +0530

    String implementation

---
+++ Arrays_And_String/Strings/myString.java
@@ -0,0 +1,13 @@
+//java program to demonstrate string implementation
+package Arrays_And_String.Strings;
+
+public class myString {
+    public static void main(myString[] args)
+    {
+        String str1 = "Rohan";          //By String literal
+        String str2 = new String("Pinaki");     //By new keyword
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
