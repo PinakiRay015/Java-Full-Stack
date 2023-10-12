11afa646e414962bbc71f976115870183228c88d
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-12 19:38:44 +0530

    Arrays and Strings

---
+++ Arrays_And_String/Arrays/array.java
@@ -0,0 +1,15 @@
+//java program to demonstrate how to create an array , store data inside array and displaying an element of array
+package Arrays_And_String.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+        //implementation of array
+        int []marks = {19 , 18 , 3 , 12 , 9};  //marks of a student out of 20 in 5 different subs
+
+        //displaying a single element in array
+        System.out.println(marks[2]);
+        //displaying the size of an array
+        System.out.println(marks.length);
+
+    }
+}
