a3cd68816552427dcc3330643ba9cb9fd7dd0cc0
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-12 13:03:16 +0530

    Java variables and data types

---
+++ Variables_and_DataTypes/dataTypes.java
@@ -0,0 +1,14 @@
+package Variables_and_DataTypes;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        int a = 34;                 //Integer type data
+        String b = "Rohan";         //String type data
+        char  c = 'r';              //Character type data used to store character
+        Boolean d = false;          //Boolean type data used to store True or False
+        byte e = 34;                //Byte type data
+        short f = 2;                //Short type data
+        float g = 5.6f;             //float type data used to store fraction number
+        double dd = 67.4;           //Double type data used to store fraction number
+    }
+}
