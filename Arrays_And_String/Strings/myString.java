6622a0d3d11f4ca3a6afb85490e69aa38fc95b15
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-12 20:06:15 +0530

    String implementation

--- Arrays_And_String/Strings/myString.java
+++ Arrays_And_String/Strings/myString.java
@@ -2,12 +2,15 @@
 package Arrays_And_String.Strings;
 
 public class myString {
-    public static void main(myString[] args)
+    public static void main(String[] args)
     {
         String str1 = "Rohan";          //By String literal
         String str2 = new String("Pinaki");     //By new keyword
 
         System.out.println(str1);
         System.out.println(str2);
+
+        //concatenating Strings (means to merge or join two string together)
+        System.out.println(str1+str2);
     }
 }
