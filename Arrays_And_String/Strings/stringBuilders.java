e182c5902023b758d903d01fff536e282398050d
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-13 13:15:10 +0530

    StringBuilder implementation

--- Arrays_And_String/Strings/stringBuilders.java
+++ Arrays_And_String/Strings/stringBuilders.java
@@ -5,10 +5,5 @@ public class stringBuilders {
     {
         StringBuilder sb = new StringBuilder("Pinaki");
         System.out.println(sb);
-        sb.append(" sankar"); //Adding (concatenating) a new string to the existing string
-        System.out.println(sb);
-
-        sb = new StringBuilder("Hello developers");
-        System.out.println(sb); //updating the complete string
     }
 }
