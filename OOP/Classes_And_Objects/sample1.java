4ae291d3c87cee33b25cd6457eb660ce59bf6144
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-17 22:45:28 +0530

    Initializing classes and objects

--- OOP/Classes_And_Objects/sample1.java
+++ OOP/Classes_And_Objects/sample1.java
@@ -7,24 +7,26 @@ public class sample1 {
     public static void main(String[] args)
     {
         student st = new student();
-        System.out.println("Enter the name of the student");
-        System.out.println("The name of the student is "+st.name());
-        System.out.println("Enter the ID of the student");
-        System.out.println("The id of the student is "+st.id());
+        st.getDetails();
+        st.displayDetails();
     }
 }
 
 class student{
-    int student_id;
+    int student_roll;
     String student_name;
     Scanner sc = new Scanner(System.in);
-    public String name()
+    public void getDetails()
     {
-        return student_name = sc.nextLine();
+        System.out.println("Enter the name of the student");
+        student_name = sc.nextLine();
+        System.out.println("Enter the roll number of the student");
+        student_roll = sc.nextInt();
     }
 
-    public int id()
+    public void displayDetails()
     {
-        return student_id = sc.nextInt();
+        System.out.println("The name of the student is "+student_name);
+        System.out.println("The roll number of the student is "+student_roll);
     }
 }
