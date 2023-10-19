78efe39ab259a280196959198fe485272afcb384
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-10-19 19:39:50 +0530

    multi level inheritance

---
+++ OOP/Inheritance/multiLevel.java
@@ -0,0 +1,38 @@
+//java program to demonstrate multi level inheritance
+package OOP.Inheritance;
+
+class mobile
+{
+    public void network()
+    {
+        System.out.println("comes with 5g chipset");
+    }
+}
+
+class iphone15 extends mobile
+{
+    public void os()
+    {
+        super.network();
+        System.out.println("It has iOS");
+    }
+}
+
+class samsungS23 extends mobile
+{
+    public void os()
+    {
+        super.network();
+        System.out.println("It has android OS");
+    }
+}
+public class multiLevel {
+    public static void main(String[] args) {
+
+        iphone15 iphn = new iphone15();
+        samsungS23 s23 = new samsungS23();
+
+        iphn.os();
+        s23.os();
+    }
+}
